ed530f6bf2344b44415840eb7e8d26967505b2d5
Victor Arcas <victor.arcas@enti.cat>
2025-01-28 11:16:39 +0100

    Falta acabar dahs
    Falta completar codigo

--- Assets/Movimiento.cs
+++ Assets/Movimiento.cs
@@ -4,34 +4,49 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    float horizontal;
+    public float horizontal;
     float vertical;
     public float speed;
     public float impulso;
     public bool suelo;
-
     public Rigidbody2D rb;
-    
+    public Collider2D player;
+    private Dash playerdash; 
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerdash = GetComponent<Dash>();
+        player = GetComponent<Collider2D>();
+        player.isTrigger = false;
+        playerdash = GetComponent<Dash>();
     }
 
     // Update is called once per frame
     void Update()
     {
-   
         horizontal = Input.GetAxis("Horizontal");
-        
         Vector3 direccion = new Vector3(horizontal, 0);
-
         transform.position += direccion * speed * Time.deltaTime;
-
         if(Input.GetKeyDown(KeyCode.UpArrow) && suelo)
         {
             rb.AddForce(Vector2.up * impulso, ForceMode2D.Impulse);
             suelo = false;
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            player.isTrigger = true;
+        }
+        else
+        {
+            player.isTrigger = false;
+        }
+
+        if (playerdash.Isdashing)
+        {
+            
         }
     }
     void OnCollisionEnter2D(Collision2D collision){
