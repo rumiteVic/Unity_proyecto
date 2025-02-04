b769358fcf1b62e22bd382fc08a34d40077d12ef
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2025-02-04 11:13:18 +0100

    jaula
    una jaula a la que le faltan cosas

--- Assets/script/Habilidades_Luz.cs
+++ Assets/script/Habilidades_Luz.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Habilidades_Luz : MonoBehaviour
 {
     public GameObject muroDeLuz;
+    public GameObject jaula;
     float dirige = 2;
     float izDe;
     float horizontal;
@@ -40,9 +41,21 @@ public class Habilidades_Luz : MonoBehaviour
             }
             if (muro)
             {
-                Vector2 direccion = new Vector2(transform.position.x + dirige * izDe, transform.position.y);
+                Vector2 direccion = new Vector2(transform.position.x + dirige * izDe, transform.position.y + 1.5f);
                 GameObject tempMuro = Instantiate(muroDeLuz, direccion, transform.rotation);
                 muro = false;
+                Destroy(tempMuro, 7);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            muro = true;
+
+        }
+        if (muro)
+        {
+            Vector2 direccion = new Vector2(transform.position.x + dirige * izDe, transform.position.y);
+            GameObject tempMuro = Instantiate(jaula, direccion, transform.rotation);
+            muro = false;
             Destroy(tempMuro, 7);
         }
     }
--- Assets/script/Movimiento.cs
+++ Assets/script/Movimiento.cs
@@ -18,7 +18,6 @@ public class Movimiento : MonoBehaviour
     public GameObject player1;
     
 
-
     // Start is called before the first frame update
     void Start()
     {
