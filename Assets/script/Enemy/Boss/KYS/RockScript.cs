0f5c943b89c1aad84f1ac53fcd409a19671b3ae2
Victor Arcas <victor.arcas@enti.cat>
2025-05-05 22:22:43 +0200

    Nivel 3 en proceso
    a

--- Assets/script/Enemy/Boss/KYS/RockScript.cs
+++ Assets/script/Enemy/Boss/KYS/RockScript.cs
@@ -15,6 +15,7 @@ public class RockScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         counter += Time.deltaTime;
         if (counter >= 1)
         {
